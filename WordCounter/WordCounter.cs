1648b0fc708abbe2805432150dda1792cf4183c2
Edin Filipovic <edin.filipovic89@gmail.com>
2018-11-18 21:47:19 -0800

    testing

--- WordCounter/WordCounter.cs
+++ WordCounter/WordCounter.cs
@@ -1,41 +1,43 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
+// using System.Linq;
 
 namespace WordCounterMain
 {
     public class WordCounterClass
     {
-        public static int CountStringOccurrences(string word1, string words2)
+        public int CountStringOccurrences(string userInputWord, string userInputSentence)
                 {
-                    // Loop through all instances of the string 'words2'.
                     int count = 0;
-                    int i = 0;
-                    while ((i = word1.IndexOf(words2, i)) != -1)
+                    string[] words2 = userInputSentence.Split();
+                    for (int i = 0; i<words2.Length; i++)
                     {
-                        i += words2.Length;
+                        if (userInputSentence == userInputWord)
+                        {                        
                         count++;
+                        }
                     }
                     return count;
                 }
 
         public static void Main()
         {
-            Console.WriteLine("Please type a that might appear in your sentence");
-            string userInput1 = Console.ReadLine();
-            string word1 = userInput1;
+            Console.WriteLine("Please type a word that might appear in your sentence");
+            string userInputWord = Console.ReadLine();
             Console.WriteLine("Please type a sentence to compare to your word");
-            string userInput2 = Console.ReadLine();
-            string[] words2 = userInput2.Split();
+            string userInputSentence = Console.ReadLine();
+            
             // foreach (var word2 in words2)
             // {
             //     System.Console.WriteLine($"<{word1}>");
 
             //     System.Console.WriteLine($"<{word2}>");
+
             // }
             
-            Console.WriteLine("Your duplicate word count is: {0}", count);
-            Console.WriteLine("Your duplicate word count is: " + (WordCounterClass.CountStringOccurrences(word1, words2)));
+            WordCounterClass countedResults = new WordCounterClass();
+            Console.WriteLine("Your duplicate word count is: {0}", countedResults.CountStringOccurrences(userInputWord, userInputSentence));
+
         }
     }
 }
