6cb1d9fab7b1af60e8929c9b93c058835f496575
Edin Filipovic <edin.filipovic89@gmail.com>
2018-11-18 23:45:42 -0800

    updated readme

--- WordCounter.Tests/WordCounterTest.cs
+++ WordCounter.Tests/WordCounterTest.cs
@@ -11,16 +11,25 @@ namespace WordCounterMain.Tests
         {
             Assert.AreEqual("3", WordCounterClass.CheckLetter("e"));
         }
-        // [TestMethod]
-        // public void CheckNumber_InputIsO_Zero()
-        // {
-        //     Assert.AreEqual("0", WordCounterClass.CheckLetter("o"));
-        // }
-        // [TestMethod]
-        // public void CheckNumber_InputIsI_One()
-        // {
-        //     Assert.AreEqual("1", WordCounterClass.CheckLetter("I"));
-        // }
+        [TestMethod]
+        public void IsUserInputStrings_ConfirmUserInputIsString_Bool()
+        {
+            string testWord = "bread";
+            string testSentence = "the bread baked in the oven";
+            Word myWord = new Word();
+            
+            Assert.AreEqual(true, myWord.IsUserInputStrings(testWord, testSentence));
+        }        {
+            Assert.AreEqual("0", WordCounterClass.CheckLetter("o"));
+        }
+        [TestMethod]
+        public void CheckNumber_InputIsI_One()
+        {
+        string testSentence = "Snarfle Darfle";
+        RepeatCounter newRepeatCounter = new RepeatCounter(testSentence);
+        string result = newRepeatCounter.GetSentence();
+        Assert.AreEqual(testSentence, result);        
+        }
         // [TestMethod]
         // public void CheckNumber_InputIsT_Seven()
         // {
---
+++ readme.md
@@ -0,0 +1,34 @@
+# Word Counter in C#
+
+#### By Patricia Filipovic
+
+## Description
+Start of 2 part project counting words where the user enters both a word and a sentence. The program then checks the  sentence for how frequently the initial word appears in the sentence. 
+
+## Specifications
+
+* Input taken in for both fields (first field is a word and second is a sentence). 
+* The sentence is split and converted into an array inside the Counter method. 
+* A loop goes through the sentence array and after setting all case to lower, looks for matches, tabulating each occurance. 
+* Tabulation is returned to user field to be displayed on the user interface.
+ 
+##Install Instructions:
+
+- Clone from github.com on terminal using "git clone https://github.com/bkindtoall/WordCounter.git"
+
+In Terminal, In the WordCounter directory:
+- Run mcs WordCounter.cs to create executable
+- Run the executable WordCounter.exe 
+
+## Support and contact details
+
+Contact Patricia via email: bkindtoall@gmail.com
+
+## Technologies Used
+
+C#
+Visual Studio Code
+
+*This software is licensed under the MIT license.*
+
+Copyright (c) 2018 PF
